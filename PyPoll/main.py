b5c68d3289c557633ace8d1cf5c283be7061875f
Jackson Cummings <barbercummings@gmail.com>
2019-06-05 11:13:02 -0500

    made a for loop for pypoll to be a bit cleaner, done with this homework

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,7 +7,6 @@ import csv
 #import counter
 from collections import Counter
 
-
 csvpath = 'election_data.csv'
 
 #lists to store data
@@ -33,43 +32,22 @@ votes_count = len(id_list)
 
 #finding list of candidates
 candidate_names = list(set(candidate_list))
-#print(candidate_names)
-
-#finding percent and number of votes per candidate
-#Khan
-khan_num = candidate_list.count('Khan')
-khan_percent = "{:.3%}".format(khan_num / votes_count)
-#print(f"Khan: {khan_percent} ({(khan_num)})")
-
-#Correy
-correy_num = candidate_list.count('Correy')
-correy_percent = "{:.3%}".format(correy_num / votes_count)
-#print(f"Correy: {correy_percent} ({(correy_num)})")
-
-#Li
-li_num = candidate_list.count('Li')
-li_percent = "{:.3%}".format(li_num / votes_count)
-#print(f"Li: {li_percent} ({(li_num)})")
-
-#O'Tooley
-otooley_num = candidate_list.count('O\'Tooley')
-otooley_percent = "{:.3%}".format(otooley_num / votes_count)
-#print(f"O\'Tooley: {otooley_percent} ({(otooley_num)})")
-
-#finding a winner
-
-#print(f"Winner: {max(set(candidate_list), key=candidate_list.count)}")
-
 
 #print findings
 print("Election Results")
 print("----------")
 print(f"Total Votes: {votes_count}")
 print("----------")
-print(f"Khan: {khan_percent} ({(khan_num)})")
-print(f"Correy: {correy_percent} ({(correy_num)})")
-print(f"Li: {li_percent} ({(li_num)})")
-print(f"O\'Tooley: {otooley_percent} ({(otooley_num)})")
+
+#loop through each name and print count and percent
+for name in candidate_names:
+        name_num = candidate_list.count(name)
+        name_percent = "{:.3%}".format(name_num / votes_count)
+        print(f"{name}: {name_percent} ({(name_num)})")
+
 print("----------")
+
+#finding a winner
+
 print(f"Winner: {max(set(candidate_list), key=candidate_list.count)}")
 print("----------")
